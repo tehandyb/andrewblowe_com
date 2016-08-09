ebf7cc8c565e0eb53eac231380b01b18685c9e42
Andrew <tehandyb@gmail.com>
2016-08-09 04:45:28 +0000

    ejected from react boiler plate to add scss. Also made the basic page layout and a snippet component

--- src/AboutMeSnippet.js
+++ src/AboutMeSnippet.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
+import Tidbit from './Tidbit.js'
 
 class AboutMeSnippet extends Component {
   render() {
     return (
       <div className="about-me-snippet">
-        Hey you've found my personal webpage! Please explore and feel free to drop me a line if you desire. 
-        A little about me:
-        
+        Hi interweb traveler! I'm Andrew Blowe.<br/>
+        To start, here's a little tidbit about me: <br/>
+        <div className="tidbit-line">I like to <Tidbit /></div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './index.scss';
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('container')
 );
