719a3eeaa52d5e019c2f11bc72f55d437b108422
Andrew <tehandyb@gmail.com>
2016-08-09 03:00:26 +0000

    started about me snippet

---
+++ src/AboutMeSnippet.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class AboutMeSnippet extends Component {
+  render() {
+    return (
+      <div className="about-me-snippet">
+        Hey you've found my personal webpage! Please explore and feel free to drop me a line if you desire. 
+        A little about me:
+        
+      </div>
+    )
+  }
+}
+
+export default AboutMeSnippet
